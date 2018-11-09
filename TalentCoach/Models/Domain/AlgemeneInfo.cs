a33f912b1a17532eb1620b6a3bd87b447f485726
SamDhondt <sam.dhondt.w1599@student.hogent.be>
2018-11-09 14:12:29 +0100

    Attempt to fix AlgemeneInfo reference

--- TalentCoach/Models/Domain/AlgemeneInfo.cs
+++ TalentCoach/Models/Domain/AlgemeneInfo.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace TalentCoach.Models.Domain
 {
     public class AlgemeneInfo
     {
-        public AlgemeneInfo()
+        public int Id { get; set; }
+        public string Titel { get; set; }
+        public string Omschrijving { get; set; }
+
+        public AlgemeneInfo(string titel, string omschrijving)
         {
+            Titel = titel;
+            Omschrijving = omschrijving;
         }
     }
 }
