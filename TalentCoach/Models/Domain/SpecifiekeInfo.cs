8b95d397f124aac59a97d3baa99aa61f6d905b9d
SamDhondt <sam.dhondt.w1599@student.hogent.be>
2018-11-09 13:06:33 +0100

    Add model SpecifiekeInfo

--- TalentCoach/Models/Domain/SpecifiekeInfo.cs
+++ TalentCoach/Models/Domain/SpecifiekeInfo.cs
@@ -1,20 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace TalentCoach.Models.Domain
 {
-    public class AlgemeneInfo
+    public class SpecifiekeInfo
     {
         public int Id { get; set; }
         public string Titel { get; set; }
         public string Omschrijving { get; set; }
+        public Werkgever Werkgever { get; set; }
 
-        public AlgemeneInfo(string titel, string omschrijving)
+        public SpecifiekeInfo(string titel, string omschrijving, Werkgever werkgever)
         {
             Titel = titel;
             Omschrijving = omschrijving;
+            Werkgever = werkgever;
         }
     }
 }
