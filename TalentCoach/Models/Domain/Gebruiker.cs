55d2ba5fa9b3c92ea3b3fdf333643993f3650a91
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-11-27 17:38:13 +0100

    Update gebruiker and gebruikerDto

--- TalentCoach/Models/Domain/Gebruiker.cs
+++ TalentCoach/Models/Domain/Gebruiker.cs
@@ -6,6 +6,8 @@
         public string Naam { get; set; }
         public string Voornaam { get; set; }
         public string Gebruikersnaam { get; set; }
+        public GebruikersRol GebruikersRol { get; set; }
+        public int ConcreteGebruikerId { get; set; }
         public byte[] WachtwoordHash { get; set; }
         public byte[] WachtwoordSalt { get; set; }
     }
